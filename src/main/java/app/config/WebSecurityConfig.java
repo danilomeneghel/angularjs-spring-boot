9741515c58d13d3aedda6161c3ab606164ca13bb
danilo.meneghel <danilo.meneghel@gmail.com>
2019-11-27 00:15:55 -0300

    Update files

--- src/main/java/app/config/WebSecurityConfig.java
+++ src/main/java/app/config/WebSecurityConfig.java
@@ -24,7 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**", "/images/**", 
-            		"/swagger**", "/v2/**", "/cadastro", "/salvarCadastro").permitAll() 
+            		"/swagger**", "/cadastro", "/salvarCadastro").permitAll() 
             .and()
             .csrf().disable()
             .authorizeRequests()
